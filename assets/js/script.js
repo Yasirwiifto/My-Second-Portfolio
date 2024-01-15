f3f089038a4e22b69d1f4534ebf749c8b66471d6
Yasir Mohamed <Yasirwiifto@gmail.com>
2024-01-15 15:07:29 +0000

    Feat: added slides to DOM elements and created three function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
-gsap.from(".title", { duration: 4, y: "-100%", ease: "elastic"});
+// Title animation 
+gsap.from(".title", { duration: 4, y: "-12%", ease: "elastic"});
 
-gsap.from("#about-img", { duration: 4, x: "100%", ease: "elastic"});
+// About image animation
+gsap.from("#about-img", { duration: 4, x: "12%", ease: "elastic"});
+
+
+// Get references to DOM elements
+const slidesContainer = document.querySelector('.slides');
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+let currentSlide = 0;
+
+// Function to update the position of slides
+function updateSlidePosition() {
+  slidesContainer.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+// Function to navigate to the next slide
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  updateSlidePosition();
+}
+
+// Function to navigate to the previous slide
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  updateSlidePosition();
+}
+
+// Automatic slide change every 3 seconds
+setInterval(nextSlide, 3000);
